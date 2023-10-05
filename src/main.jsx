855dce60562e5d259c8b23cef308e8c5fbfcae5a
Endric Lopes <endricayl@gmail.com>
2023-10-05 13:58:38 -0300

    apagando config main

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React from 'react'
 import reactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
