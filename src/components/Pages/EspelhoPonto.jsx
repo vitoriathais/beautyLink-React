7720ddcb4165abba136dab9f3247a99e6266719c
Endric Lopes <endricayl@gmail.com>
2023-10-11 13:18:04 -0300

    grid

--- src/components/Pages/EspelhoPonto.jsx
+++ src/components/Pages/EspelhoPonto.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import '../../styles/Espelho.css'
 
 const dados = [
     { data: '10/10/2023', entradaSaida: '08:00/18:00', horasExtras: '2', horasFaltas: '0', saldo: '2', solicitacao: 'N/A', acoes: '✔️' },
@@ -7,36 +8,21 @@ const dados = [
 
 function EspelhoPonto() {
   return (
-    <div className="Espelho">
-      <h1>Meu Ponto</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Data</th>
-            <th>Entrada/Saída</th>
-            <th>Horas extras</th>
-            <th>Horas faltas</th>
-            <th>Saldo</th>
-            <th>Solicitação</th>
-            <th>Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-        {dados.map((linha, index) => (
-        <tr key={index}>
-          <td>{linha.data}</td>
-          <td>{linha.entradaSaida}</td>
-          <td>{linha.horasExtras}</td>
-          <td>{linha.horasFaltas}</td>
-          <td>{linha.saldo}</td>
-          <td>{linha.solicitacao}</td>
-          <td>{linha.acoes}</td>
-        </tr>
-      ))}
-        </tbody>
-      </table>
+    <div className="container-espelho" id="template-areas">
+      <div id="header">
+        <p>Cabçalho</p>
+      </div>
+      <div id="sidebar">
+        <p>Sidebar</p>
+      </div>
+      <div id="content">
+        <p>Conteúdo</p>
+      </div>
+      <div id="footer">
+        <p>Rodapé</p>
+      </div>
     </div>
-  );
+  )
 }
 
 export default EspelhoPonto;
