dc4388eb35557ba3e0accb81a4942ee8ab7642c6
Endric Lopes <endricayl@gmail.com>
2023-10-16 09:55:45 -0300

    grade 2

--- src/components/Grade/BlocoOpção.jsx
+++ src/components/Grade/BlocoOpção.jsx
@@ -1,6 +1,6 @@
-/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React from 'react' 
+import { useState } from 'react';
 
 const BlocoOpção = () => {
     const [isOpen, setIsOpen] = useState(false);
