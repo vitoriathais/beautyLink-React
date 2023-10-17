922152c3ce5866625a97fda5e36541214dacfad9
Endric Lopes <endricayl@gmail.com>
2023-10-17 10:22:03 -0300

    espelho black

--- src/components/Espelho/Grade/Grade.jsx
+++ src/components/Espelho/Grade/Grade.jsx
@@ -16,18 +16,20 @@ const TabelaDados = () => {
     <div className="Container_Grade">
     <table>
       <thead>
-        <tr>
+        <tr className="Container_Grade_Cabeçalho">
           {/*Nome Colunas*/}
+          <tr>Data</tr>
           <th>Horário entrada</th>
           <th>Horário saída</th>
           <th>Horas extra</th>
           <th>Saldo</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className="Container_Grade_Results">
         {dados.map((linha, index) => (
           <tr key={index}>
             {/* Substitua 'coluna1', 'coluna2', etc. pelos nomes das suas colunas */}
+            <td>{linha['Data']}</td>
             <td>{linha['Horario entrada']}</td>
             <td>{linha['Horario saída']}</td>
             <td>{linha['Horas extra']}</td>
