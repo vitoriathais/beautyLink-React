f5e57ea44aae014ecb53c2b6d386bc079db196e3
Endric Lopes <endricayl@gmail.com>
2024-09-09 22:14:58 -0300

    commit de alteração de endpoint

--- src/components/Pages/FormCadastro.jsx
+++ src/components/Pages/FormCadastro.jsx
@@ -15,7 +15,7 @@ function Cadastro() {
     }
 
     try {
-      const response = await axios.post('https://batida-de-ponto-api-flask.vercel.app/cadastro', novoUsuario);
+      const response = await axios.post('https://beauty-link-python.vercel.app/cadastro', novoUsuario);
       alert(response.data.message);
       navigate('/');
     } catch (error) {
