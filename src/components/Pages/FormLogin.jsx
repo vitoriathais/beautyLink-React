09079650becb9e83820fa3aa37486c813b7b95f9
Endric Lopes <endricayl@gmail.com>
2024-09-09 23:10:27 -0300

    arrumando url

--- src/components/Pages/FormLogin.jsx
+++ src/components/Pages/FormLogin.jsx
@@ -24,7 +24,7 @@ function FormLogin() {
             window.alert('Por favor, insira o usuário e a senha.');
             return; }
 
-        axios.post('https://batida-de-ponto-api-flask.vercel.app/Login', {
+        axios.post('https://beauty-link-react.vercel.app/Login', {
             usuario: usuario,
             senha: senha
         })
