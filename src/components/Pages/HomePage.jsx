1005e0f5fcf0944dc2ebfbe79fb9d53f59a992eb
Endric Lopes <endricayl@gmail.com>
2024-09-10 22:31:51 -0300

    atualizando css

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -2,7 +2,6 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/Home.css';
-import mulherHome from '../Imagens/mulherHome.png'; // Importação correta da imagem
 
 const Header = () => {
   return (
@@ -76,7 +75,7 @@ const ProductBar = () => {
   return (
     <div className="product-bar">
       <div className="product-item">
-        <img src={mulherHome} alt="Produto 1" /> {/* Uso correto da imagem importada */}
+        <img src="https://via.placeholder.com/300x200" alt="Produto 1" />
         <p>Produto 1</p>
       </div>
       <div className="product-item">
