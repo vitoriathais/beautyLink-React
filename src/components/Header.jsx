68b60b244f4f7a4e8553d8e96f6f22be8da67cc6
Thais Lima <113526378+vitoriathais@users.noreply.github.com>
2024-09-11 11:13:22 -0300

    correção Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,18 @@
-// src/components/Header.js
-import '../styles/Header.css'; // Importa o CSS do cabeçalho
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Header = () => {
-  const navigate = useNavigate();
-
-  const linkLogin = () => {
-    navigate('/Login');
-  };
-
+function Header() {
   return (
-    <header className="header">
-      <nav>
-        <button className="nav-button">Sobre</button>
-        <button className="nav-button">Agendamentos</button>
-        <button className="nav-button">Equipes</button>
-        <button className="nav-button">Contato</button>
-        <button className="nav-button" onClick={linkLogin}>Login</button>
-        <button className="nav-button">Cadastro</button>
+    <header className="bg-info fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-light container">
+        <div className="navbar-nav mx-auto">
+          <button className="btn btn-link nav-item nav-link text-white">Home</button>
+          <button className="btn btn-link nav-item nav-link text-white">Sobre</button>
+          <button className="btn btn-link nav-item nav-link text-white">Contato</button>
+        </div>
       </nav>
     </header>
   );
-};
+}
 
 export default Header;
